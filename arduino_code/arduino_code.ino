5d753241336778c902a77899d09e09259f7ccc87
Nicola Montecchio <nicola.montecchio@gmail.com>
2016-01-15 16:57:33 -0500

    cleanup C code

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,15 +1,13 @@
 #include <SoftwareSerial.h>
 
+int SERIAL_RX = 2;
+int SERIAL_TX = 3;
 int PL_ = 4;
 int CP = 5;
 int Q7 = 6;
-int SERIAL_RX = 2;
-int SERIAL_TX = 3;
 
+SoftwareSerial midiSerial(SERIAL_RX, SERIAL_TX);
 
-SoftwareSerial midiSerial(SERIAL_RX, SERIAL_TX); // digital pins that we'll use for soft serial RX & TX
-
-int TESTLED = 10;
 int LOWEST_MIDI_NOTE = 29;
 
 void noteOn(byte cmd, byte data1, byte data2) {
@@ -19,54 +17,42 @@ void noteOn(byte cmd, byte data1, byte data2) {
 }
 
 void setup() {
-  Serial.begin(9600);   
-  midiSerial.begin(31250);  // midi speed
-  pinMode(PL_, OUTPUT);  // PL
-  pinMode(CP, OUTPUT);  // CP
-  pinMode(Q7, INPUT);   // Q7
+  Serial.begin(9600);
+  midiSerial.begin(31250);
+  pinMode(PL_, OUTPUT);         // shift register PL
+  pinMode(CP, OUTPUT);          // shift register CP
+  pinMode(Q7, INPUT);           // shift register Q7
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-//  Serial.println("sampling state");
-  digitalWrite(PL_, LOW);
 
+  // load in shift register
+  digitalWrite(PL_, LOW);
   digitalWrite(CP, LOW);
-  
-  delay(1); 
-//  Serial.println("reading state");
+  delay(1);
   digitalWrite(PL_, HIGH);
 
   delay(1);
   int NIN = 8;
   int inputs[NIN];
 
-  for(int i = 0; i < NIN; i++) {
-    // read 
+  for(int i = NIN - 1; i >= 0; i--) {
+
+    // read
     int newValue = digitalRead(Q7);
-    int oldValue = inputs[NIN-1-i];
+    int oldValue = inputs[i];
     if(newValue != oldValue) {
       if(newValue == HIGH)
-        noteOn(0x90, LOWEST_MIDI_NOTE + NIN - i + 1, 127);
-      else 
-        noteOn(0x90, LOWEST_MIDI_NOTE + NIN - i + 1, 0);      
-      inputs[NIN-1-i] = newValue;    
-      Serial.print("note ");
-      Serial.print(LOWEST_MIDI_NOTE + i);
-      Serial.print(": ");
-      Serial.print(newValue);
-      Serial.println("");
+        noteOn(0x90, LOWEST_MIDI_NOTE + i, 127);
+      else
+        noteOn(0x90, LOWEST_MIDI_NOTE + 1, 0);
+      inputs[i] = newValue;
     }
+
     // shift
     digitalWrite(CP, LOW);
     digitalWrite(CP, HIGH);
   }
-  
-//  if(inputs[0] == 1)
-//    digitalWrite(TESTLED, HIGH);
-//  else
-//    digitalWrite(TESTLED, LOW);
-//   
+
   delay(10);
 }
